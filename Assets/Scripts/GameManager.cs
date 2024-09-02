302e0f7c50439f127f86fb8cf0d7c5ca1b95b156
Kaldorei00910 <117884183+Kaldorei00910@users.noreply.github.com>
2024-09-02 17:01:43 +0900

    add / ObjectPool,GameManager
    싱글톤 GameManager
    오브젝트 최적화 objectPool
    추가함

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,47 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    
+    public static GameManager _instance;
+
+    private Player _player;
+    public Player Player { get { return _player; } set { _player = value; } }
+    public ObjectPool ObjectPool { get; private set; }
+
+
+
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = new GameObject("GameManager").AddComponent<GameManager>();
+            }
+            return _instance;
+        }
+    }
+    private void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this;
+            ObjectPool = GetComponent<ObjectPool>();
+
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            if (_instance != null)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+
+
 }
